61635305d785e7cc5e8f12537c3be952e832577d
luquibarba <lb588071@gmail.com>
2025-03-13 15:27:43 -0300

    subo ej 4

---
+++ 13-3/ej4.py
@@ -0,0 +1,7 @@
+xd = 0.2 + 0.1
+dx = 3 * 0.1
+alt = 1 - 0.9
+print (xd, dx, alt)
+
+#El resultado va a contener al final (luego de un par de comas) un 4
+#Esto sucede debido al formato que utilizan las computadoras, ya que no puede ser completamente exacto, y al redondear ese numero lo mas exacto posible suceden estos errores
