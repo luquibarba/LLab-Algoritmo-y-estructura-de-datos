db5ad5b9784d8b6cf038dde42f5aaf9e2343a3b9
luquibarba <lb588071@gmail.com>
2025-03-13 15:14:49 -0300

    agrego ej 3

--- 13-3/ej3.py
+++ 13-3/ej3.py
@@ -1,2 +1,9 @@
-dasdasd = "   runa w  a"
-print (dasdasd.strip())
+dasdasd = input("Introduce algo:") #Introducir alguna palabra
+xd = dasdasd.isdigit() #Lo guardamos en "xd"
+if xd == True: #Controla si lo introducido contiene o no digitos
+    print ("Lo que ha introducido contiene digitos")
+else:    
+    print ("Lo que ha introducido contiene digitos")
+
+print ("Lo que ha introducido fue:" , dasdasd)
+print ("La primera letra en mayus:", dasdasd.capitalize()) #Finalmente printea la variable en mayusculas
