f32fd039895ad887a8a9a7db2db77b2ca7a0f01a
luquibarba <lb588071@gmail.com>
2025-03-19 11:28:09 -0300

    8.2

---
+++ 19 - 3/ej 8-2.py
@@ -0,0 +1,13 @@
+lista = ["juan", "pepe", "ricky", "centurion"]
+print (f"Hola",(lista[0]),". Te invito mañana para venir a comer a casa. Avisame!")
+print (f"Hola",(lista[1]),". Te invito mañana para venir a comer a casa. Llamame!")
+print (f"Hola",(lista[2]),". Te invito mañana para venir a comer a casa. Hablame!")
+print (f"Hola",(lista[3]),". Te invito mañana para venir a comer a casa.")
+
+print (f"",(lista[2]), "no puede ir a comer mañana")
+lista[3] = input ("Ingresa al nuevo invitado:")
+
+print (f"Hola",(lista[0]),". Te invito mañana para venir a comer a casa. Avisame!")
+print (f"Hola",(lista[1]),". Te invito mañana para venir a comer a casa. Llamame!")
+print (f"Hola",(lista[2]),". Te invito mañana para venir a comer a casa. Hablame!")
+print (f"Hola",(lista[3]),". Te invito mañana para venir a comer a casa.")
---
+++ 19 - 3/ej8.py
@@ -0,0 +1,5 @@
+lista = ["juan", "pepe", "kanye", "jose"]
+print (f"Hola",(lista[0]),"! Te invito mañana para venir a comer a casa. Avisame!")
+print (f"Hola",(lista[1]),"! Te invito mañana para venir a comer a casa. Llamame!")
+print (f"Hola",(lista[2]),"! Te invito mañana para venir a comer a casa. Hablame!")
+print (f"Hola",(lista[3]),"! Te invito mañana para venir a comer a casa.")
