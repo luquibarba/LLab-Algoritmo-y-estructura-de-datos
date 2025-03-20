74ae36b14d8886a88b74d3f12394374fd83924fa
luquibarba <lb588071@gmail.com>
2025-03-20 15:28:48 -0300

    13

---
+++ 20 - 3/ej 13.py
@@ -0,0 +1,11 @@
+color_alien = "rojo"
+if color_alien == "verde":
+    print("Bien! Ganaste 5 puntos")
+else:
+    print("No ganaste nada")
+
+color_alien = "verde"
+if color_alien == "verde":
+    print("Bien! Ganaste 5 puntos")
+else:
+    print("")
