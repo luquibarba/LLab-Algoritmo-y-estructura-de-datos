aa1c6c59488f91ab750c08346b9b2360dc682297
luquibarba <lb588071@gmail.com>
2025-03-20 15:47:53 -0300

    xd

---
+++ 20 - 3/ej 13-1.py
@@ -0,0 +1,11 @@
+color_alien = "rojo"
+if color_alien == "verde":
+    print("Bien! Ganaste 5 puntos")
+else:
+    print("Bien! Ganaste 10 puntos")
+
+color_alien = "verde"
+if color_alien == "verde":
+    print("Bien! Ganaste 5 puntos")
+else:
+    print("Bien! Ganaste 10 puntos")
---
+++ 20 - 3/ej 13-3.py
@@ -0,0 +1,23 @@
+color_alien = "verde"
+if color_alien == "verde":
+    print("Bien! Ganaste 5 puntos")
+elif color_alien == "amarillo":
+    print("Bien! Ganaste 10 puntos")
+else:
+    print("Bien! Ganaste 15 puntos")
+
+color_alien = "amarillo"
+if color_alien == "verde":
+    print("Bien! Ganaste 5 puntos")
+elif color_alien == "amarillo":
+    print("Bien! Ganaste 10 puntos")
+else:
+    print("Bien! Ganaste 15 puntos")
+
+color_alien = "rojo"
+if color_alien == "verde":
+    print("Bien! Ganaste 5 puntos")
+elif color_alien == "amarillo":
+    print("Bien! Ganaste 10 puntos")
+else:
+    print("Bien! Ganaste 15 puntos")
