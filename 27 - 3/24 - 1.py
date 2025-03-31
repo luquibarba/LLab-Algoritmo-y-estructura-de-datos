1b729c2d8997216bc74afda26e1b76eee14f06e9
luquibarba <lb588071@gmail.com>
2025-03-30 23:13:16 -0300

    30 - 3

---
+++ 27 - 3/24 - 1.py
@@ -0,0 +1,6 @@
+msg = ["hola", "xd", "como andas?"]
+def mostrar_mesnajes():
+    for m in msg:
+        print(m)
+
+mostrar_mesnajes()
---
+++ 27 - 3/24 - 2.py
@@ -0,0 +1,16 @@
+msg = ["hola", "xd", "como andas?"]
+def mostrar_mesnajes():
+    for m in msg:
+        print(m)
+mostrar_mesnajes()
+
+msg_enviados = []
+def enviar_mensajes():
+    while msg:
+        mensaje = msg.pop()
+        msg_enviados.append(mensaje)
+    print (msg)
+    print (msg_enviados)
+
+enviar_mensajes()
+
